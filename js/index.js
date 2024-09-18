31ad9102e64d840b46e75bcdcd96d6e7287d7cb8
adenekansubomi <129598884+adenekansubomi@users.noreply.github.com>
2024-09-18 13:15:59 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,25 @@ harmbuger.addEventListener("click", ()=>{
     navMenu.classList.toggle("active");
 })
 
+document.addEventListener('DOMContentLoaded', function() {
+  var subscribeButton = document.getElementById('subscribeButtonf');
+  var emailInput = document.getElementById('emailInputf');
+
+  subscribeButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var email = emailInput.value;
+    var atIndex = email.indexOf("@");
+    var com = email.indexOf(".com");
+
+    if (atIndex !== -1 && com !== -1 && email.length > 8) {
+      alert('Thank you for subscribing!');
+    } else {
+      alert('Please enter a valid email address.');
+    }
+  });
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var subscribeButton = document.getElementById('subscribeButton');
